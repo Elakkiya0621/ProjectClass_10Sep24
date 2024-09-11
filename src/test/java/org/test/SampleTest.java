f58322ac0ed890f5fa3e773538ed25bb7c07e674
KowsalyaBaskar <Kowsalyab2611@gmail.com>
2024-09-11 14:42:07 +0530

    kowsi code

--- src/test/java/org/test/SampleTest.java
+++ src/test/java/org/test/SampleTest.java
@@ -3,7 +3,9 @@ package org.test;
 public class SampleTest {
 
 	public static void main(String [] args) {
-		System.out.println("Test");
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
 		//here start to write implementations
 	}
 }
