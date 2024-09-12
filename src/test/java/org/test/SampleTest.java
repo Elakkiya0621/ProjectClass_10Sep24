f59e59db82c60e10839d0abc26c4f641140f1289
KowsalyaBaskar <kowsalyab2611@gmail.com>
2024-09-12 13:13:59 +0530

    kowsi's code-2

--- src/test/java/org/test/SampleTest.java
+++ src/test/java/org/test/SampleTest.java
@@ -6,6 +6,9 @@ public class SampleTest {
 		System.out.println("Test1");
 		System.out.println("Test2");
 		System.out.println("Test3");
+		System.out.println("Test7");
+		System.out.println("Test8");
+		System.out.println("Test9");
 		//here start to write implementations
 	}
 }
